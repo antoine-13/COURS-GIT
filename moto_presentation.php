315addc12ef3aabc6cdd84afba2758df24a048b3
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-03-01 15:13:21 +0100

    Ajout des headers et footers, ajout d'un petit texte

--- moto_presentation.php
+++ moto_presentation.php
@@ -1,1 +1,13 @@
 <?php
+
+
+require_once ('includes/front/header.php');
+?>
+
+    <h1>La supériorité des motos</h1>
+
+    <p>Nous nous sommes tous déjà demandé quelle était la différence fondamentale entre une moto est un scooter. Ici, nous allons voir en quoi une moto est
+    largement supérieure à un scooter</p>
+
+<?php
+require_once ('includes/front/footer.php');
