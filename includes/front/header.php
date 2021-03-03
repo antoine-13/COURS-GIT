ff8b27ce0ea923a8e2a1ea333c58e068b912b864
oualid3 <73546426+oualid3@users.noreply.github.com>
2021-03-03 11:00:23 +0100

    header

---
+++ includes/front/header.php
@@ -0,0 +1,10 @@
+<img class="logo" src="./assets/img/moto.png" alt="logo">
+<nav>
+    <ul class="nav_links">
+        <li><a href="./presentation.php">Presentation</a></li>
+        <li><a href="">Antoine</a></li>
+        <li><a href="">Cedric</a></li>
+        <li><a href="">Alex</a></li>
+        <li><a href="">Oualid</a></li>
+    </ul>
+</nav>
