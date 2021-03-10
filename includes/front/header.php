85d2c7a4c5d11eb1e33ace8b0c444fe826eb5e49
antoine-13 <fouvryantoine@gmail.com>
2021-03-10 18:50:39 +0100

    new nav bar

--- includes/front/header.php
+++ includes/front/header.php
@@ -1,8 +1,9 @@
-<img class="logo" src="../../assets/img/moto.png" alt="logo">
+<img class="logo" src="../../assets/img/logo.gif" alt="logo" width="15%">
 <nav>
-    <ul class="nav_links">
-        <li><a href="../../index.php">Moto presentation</a></li>
+    <ul class="links">
         <li><a href="R1-PAGE.php">R1</a></li>
-        <li><a href="KTM.php">KTM1290R</a></li>
+        <li><a href="KTM.php">KTM</a></li>
+        <li><a href="../../index.php">Presentation</a></li>
     </ul>
 </nav>
+<a class="contact" href="#"><button>Contact</button></a>
--- index.php
+++ index.php
@@ -10,14 +10,15 @@
 </head>
 <body>
    <header>
-      <img class="logo" src="assets/img/moto.png" alt="logo">
+         <img class="logo" src="assets/img/logo.gif" alt="logo" width="15%">
       <nav>
-         <ul class="nav_links">
-            <li><a href="index.php">Moto presentation</a></li>
+         <ul class="links">
             <li><a href="includes/front/R1-PAGE.php">R1</a></li>
-            <li><a href="includes/front/KTM.php">KTM1290R</a></li>
+            <li><a href="includes/front/KTM.php">KTM</a></li>
+            <li><a href="#">Presentation</a></li>
          </ul>
       </nav>
+      <a class="contact" href="#"><button>Contact</button></a>
    </header>
 
    <p>NC700S</p>
